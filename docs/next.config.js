6909989396de260673b926b86accab8047bd9878
mollybeach <mollyjbeach@gmail.com>
2024-11-14 16:57:13 -0800

    next.config export

--- docs/next.config.js
+++ docs/next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     NEXT_PUBLIC_THIRDWEB_SECRET_KEY: process.env.NEXT_PUBLIC_THIRDWEB_SECRET_KEY
   },
   distDir: 'out',
-  output: 'next',
+  output: 'export',
   experimental: {
     forceSwcTransforms: true
   },
